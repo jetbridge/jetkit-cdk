0700cc979ae91d4610094a1842a5603f7bccedb9
Mischa Spiegelmock <me@mish.dev>
2021-05-20 22:12:49 +0300

    clean

--- src/app/config.ts
+++ src/app/config.ts
@@ -6,30 +6,9 @@
  * @module
  */
 
-import { CorsPreflightOptions } from "@aws-cdk/aws-apigatewayv2"
-import { BundlingOptions } from "@aws-cdk/aws-lambda-nodejs"
-import { default as convict } from "convict"
+export { CorsHttpMethod, CorsPreflightOptions } from "@aws-cdk/aws-apigatewayv2"
 
-export { CorsPreflightOptions, CorsHttpMethod } from "@aws-cdk/aws-apigatewayv2"
-
-export interface IConfigGeneric {
-  api?: {
-    corsPreflight?: CorsPreflightOptions
-  }
-  bundle?: BundlingOptions
-}
+// currently unused
+export interface IConfigGeneric {}
 
 export type IConfig<T = Record<string | number | symbol, unknown>> = IConfigGeneric & T
-
-const defaults: IConfig = {
-  api: {
-    corsPreflight: {},
-  },
-}
-// const schema = convict(defaults)
-
-export class Config<T> {
-  constructor(private readonly config: Partial<IConfig<T>>) {
-    // merge with defaults
-  }
-}
