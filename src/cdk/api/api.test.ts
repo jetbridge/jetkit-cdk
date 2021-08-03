bf5a5a89f3f4ba85872b018b06b85c2f436d6e12
Mischa Spiegelmock <mischa.spielgelmock@invitae.com>
2021-08-03 20:28:10 +0300

    rm bogus tests

--- src/cdk/api/api.test.ts
+++ src/cdk/api/api.test.ts
@@ -1,5 +1,5 @@
 import "@aws-cdk/assert/jest"
-import { HttpApi, HttpMethod } from "@aws-cdk/aws-apigatewayv2"
+import { HttpApi } from "@aws-cdk/aws-apigatewayv2"
 import { Stack } from "@aws-cdk/core"
 import * as path from "path"
 import { ApiView, JetKitLambdaFunction } from "./api"
@@ -24,53 +24,5 @@ describe("ApiView", () => {
 
     addRoutesSpy.mockReturnValue([])
     expect(addRoutesSpy).not.toHaveBeenCalled()
-  })
-
-  it("creates a route with any method", () => {
-    const addRoutesSpy = jest.spyOn(httpApi, "addRoutes")
-
-    new ApiView(stack, "V1", {
-      path: "/x",
-      httpApi,
-      handlerFunction,
-      methods: [],
-    })
-
-    const view = new ApiView(stack, "V2", {
-      path: "/a",
-      httpApi,
-      handlerFunction,
-      // methods defaults to [ANY]
-    })
-
-    expect(addRoutesSpy).toHaveBeenCalledWith({
-      path: "/a",
-      methods: [HttpMethod.ANY],
-      integration: view.lambdaApiIntegration,
-    })
-  })
-
-  it("creates a route with methods", () => {
-    const addRoutesSpy = jest.spyOn(httpApi, "addRoutes")
-
-    new ApiView(stack, "V1", {
-      path: "/x",
-      httpApi,
-      handlerFunction,
-      methods: [],
-    })
-
-    const view = new ApiView(stack, "V2", {
-      path: "/a",
-      httpApi,
-      handlerFunction,
-      methods: [HttpMethod.GET, HttpMethod.POST],
-    })
-
-    expect(addRoutesSpy).toHaveBeenCalledWith({
-      path: "/a",
-      methods: [HttpMethod.GET, HttpMethod.POST],
-      integration: view.lambdaApiIntegration,
-    })
   })
 })
